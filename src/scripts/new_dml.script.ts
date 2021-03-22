34264b284de8e305ec599f87739039a3d56ddda8
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-03-22 01:56:16 -0300

    chore(scripts): creating script to create scripts

---
+++ src/scripts/new_dml.script.ts
@@ -0,0 +1,58 @@
+import * as readline from 'readline';
+import * as fs from 'fs';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function getFileDir(scriptName: string, databaseSystem: string): string {
+
+    scriptName = scriptName.toLowerCase();
+    databaseSystem = databaseSystem.toLowerCase();
+
+    const date = new Date().toISOString();
+    const scriptID = date.split('.')[0].replace(/-/g, '').replace(/:/g, '').replace('T', '');
+    
+    return `src/database/${databaseSystem}/scripts/${scriptID}_${scriptName}.script.ts`;
+}
+
+function getFileContent(databaseSystem: string): string {
+
+    databaseSystem = databaseSystem.toLocaleLowerCase();
+
+    return `
+import { ${databaseSystem}Config } from "src/database";
+import { createConnection } from "typeorm";
+
+createConnection(${databaseSystem}Config)
+    .then(async connection => {
+
+        const repository = connection.getRepository(<Entity>);
+
+        // Processos a serem executados
+        // ...
+
+        process.exit();
+    })
+    .catch(error => {
+
+        console.error(error);
+
+        process.exit();
+    });
+    `;
+}
+
+rl.question("Nome do script: ", scriptName => {
+    
+    rl.question("Nome da base: ", databaseSystem => {
+
+        const fileDir = getFileDir(scriptName, databaseSystem);
+        const fileContent = getFileContent(databaseSystem);
+
+        fs.writeFileSync(fileDir, fileContent);
+
+        rl.close();
+    });
+});
