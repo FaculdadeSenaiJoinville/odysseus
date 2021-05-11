6cf4f77cc97e6a3d5f0eb056ad189f7bf94872ab
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-05-11 20:15:22 -0300

    editing README and removing console.log

--- src/common/pipes/yup-validation.pipe.ts
+++ src/common/pipes/yup-validation.pipe.ts
@@ -7,9 +7,6 @@ export class YupValidationPipe implements PipeTransform {
 	constructor(private yupSchema: Yup.ObjectSchema<any>) {}
 
 	public async transform(body: any) {
-
-        console.log(body);
-        console.log(this.yupSchema);
         
 		return this.yupSchema.validate(body).catch(response => {
 
