62fc71863d7c951c507d3e85188e52f5e510b3cc
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-06-21 20:21:34 -0300

    chore(eslint): adding new rule #PES-56

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,9 @@ module.exports = {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
-		'quotes': ["error", "single", { "allowTemplateLiterals": true, "avoidEscape": true }],
-		'indent': ["error", "tab"],
+		'quotes': ['error', 'single', { 'allowTemplateLiterals': true, 'avoidEscape': true }],
+		'indent': ['error', 'tab'],
+		'comma-dangle': ['error', 'never'],
 		'@hapi/hapi/scope-start': ['error']
 	},
 };
