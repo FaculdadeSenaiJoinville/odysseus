ac52ea2d79ad872c55585abf96086f202d5b3016
Matheus Santos <matheus.santos@rotaexata.com.br>
2021-08-30 23:31:36 -0300

    feature(pagination) Creating pagination core

---
+++ src/core/repositories/pagination/pagination.helper.ts
@@ -0,0 +1,30 @@
+import { IPagination } from './pagination.type';
+
+export function paginate(pagination?: IPagination) {
+
+    if (pagination) {
+
+        const { page = 1, perPage = 20 } = pagination;
+
+        return {
+            skip: [0, 1].includes(page) ? 0 : (page - 1) * perPage,
+            take: perPage
+        };
+    }
+
+    return {};
+}
+
+export function sort(pagination?: IPagination) {
+
+    const order = {};
+
+    if (pagination && pagination.sortField) {
+
+        const { sortField, sortOrder } = pagination;
+
+        order[sortField] = sortOrder ? sortOrder.toUpperCase() : 'ASC';
+    }
+
+    return order;
+}
