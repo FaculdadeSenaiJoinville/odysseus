d3ab9d34274af6178af461bb78900f0ada15edda
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-10-11 19:53:50 -0300

    feature(chatbot): adding google apis package #PES-316

--- src/modules/chatbot/dialogflow/dialogflow.service.ts
+++ src/modules/chatbot/dialogflow/dialogflow.service.ts
@@ -1,5 +1,6 @@
 import { HttpService, Injectable } from '@nestjs/common';
-import { DIALOGFLOW_CREDENTIALS } from './utils/dialogflow.config';
+import { DIALOGFLOW_AUTH_SCOPES, DIALOGFLOW_CREDENTIALS, GOOGLE_APIS_ENPOINTS } from './utils/dialogflow.config';
+import { google } from 'googleapis';
 
 @Injectable()
 export class DialogflowService {
@@ -8,13 +9,28 @@ export class DialogflowService {
 
 	private async getApiKey() {
 
-		return 'aaaaaa';
+		const teste = new google.auth.GoogleAuth({
+			scopes: DIALOGFLOW_AUTH_SCOPES,
+			credentials: {
+				client_id: DIALOGFLOW_CREDENTIALS.client_id,
+				private_key: DIALOGFLOW_CREDENTIALS.private_key,
+				client_email: DIALOGFLOW_CREDENTIALS.client_email,
+				type: DIALOGFLOW_CREDENTIALS.type,
+				token_url: GOOGLE_APIS_ENPOINTS.token_url,
+				quota_project_id: DIALOGFLOW_CREDENTIALS.project_id
+			}
+		});
+
+		return teste;
 	}
 
 	public async createIntent(intentBody) {
 
 		const apiKey = await this.getApiKey();
-		const createIntentUrl = `https://dialogflow.googleapis.com/v2/projects/${DIALOGFLOW_CREDENTIALS.project_id}/agent/intents?key=${apiKey}`;
+		const createIntentUrl = `https://dialogflow.googleapis.com/v2/projects/${DIALOGFLOW_CREDENTIALS.project_id}/agent/intents`;
+
+		const teste = await apiKey.getRequestHeaders(createIntentUrl);
+
 
 		return this.httpService.post(createIntentUrl, intentBody).toPromise();
 	}
--- src/modules/chatbot/dialogflow/utils/dialogflow.config.ts
+++ src/modules/chatbot/dialogflow/utils/dialogflow.config.ts
@@ -16,11 +16,10 @@ export const DIALOGFLOW_CREDENTIALS: DialogflowCredentials = {
 	api_key: process.env.BOT_API_KEY
 };
 
-export const DIALOGFLOW_ENPOINTS = {
-	auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-	token_uri: 'https://oauth2.googleapis.com/token',
-	auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-	create_intent: `${dialogflow_api_uri}/v2/projects/${DIALOGFLOW_CREDENTIALS.project_id}}/agent/intents`
+export const GOOGLE_APIS_ENPOINTS = {
+	auth_url: 'https://accounts.google.com/o/oauth2/auth',
+	token_url: 'https://oauth2.googleapis.com/token',
+	auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs'
 };
 
 export const DIALOGFLOW_AUTH_SCOPES = [
