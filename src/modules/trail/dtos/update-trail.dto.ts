0ed47089381fc6bb541ea890a0e3a0ef29855dc6
mchbaade <michel_baade@estudante.sc.senai.br>
2022-05-19 19:24:33 -0300

    update(trails): removendo código inutilizado
    update(trails): removendo código inutilizado

--- src/modules/trail/dtos/update-trail.dto.ts
+++ src/modules/trail/dtos/update-trail.dto.ts
@@ -1,16 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Group } from '../../../core/database/entities';
-
-const groupsExample = [
-	{
-		id: '675e4ed9-2758-4ba1-b596-e00fb81e2df0',
-		name: 'Grupo de teste 1'
-	},
-	{
-		id: '875e4ej9-9858-4ba1-c506-e00fb81a6dj9',
-		name: 'Grupo de teste 2'
-	}
-];
 
 export class UpdateTrailDTO {
 
